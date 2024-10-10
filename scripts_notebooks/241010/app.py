cc6366777fd8b3b78d18af16468f21fc8efb8387
Michiel van der Ree <mhvdr@protonmail.com>
2024-10-10 15:06:05 +0200

    Add gradio app

---
+++ scripts_notebooks/241010/app.py
@@ -0,0 +1,77 @@
+from openai import OpenAI
+import gradio as gr
+import yaml
+
+from llama_index.indices.managed.bge_m3 import BGEM3Index
+
+
+# Load config from YAML file
+with open("config.yaml", "r") as config_file:
+    CONFIG = yaml.safe_load(config_file)
+
+
+def context_from_nodes(nodes):
+    return "\n\n".join(
+        f"### Context item {idx}\n{node.text}" for idx, node in enumerate(nodes, start=1)
+    )
+
+class RagWrapper:
+
+    def __init__(self, client, system_prompt, retriever):
+        self.client = client
+        self.system_prompt = system_prompt
+        self.retriever = retriever
+
+        self.prompt_template = """
+Context items are below:
+```
+{context_items}
+```
+Given the context information, answer the query.
+Do not base your answer on information outside of the provided context. If the provided context does not contain any relevant information, reply with something along the lines of "Sorry, I can't help you with that."
+In your answer, don't mention term like 'context' or 'context item' when referring to the items above. Example: rather than "Based on the provided context ...", say "Based on my knowledge ...".
+Query: {query}
+    """
+
+    def predict(self, query, history):
+        nodes = self.retriever.retrieve(query)
+        context = context_from_nodes(nodes)
+        prompt = self.prompt_template.format(context_items=context, query=query)
+
+        history_openai_format = []
+        history_openai_format.append({"role": "system", "content": self.system_prompt})
+        history_openai_format.append({"role": "user", "content": prompt})
+
+        response = self.client.chat.completions.create(
+            model=CONFIG["model"],
+            messages=history_openai_format,
+            temperature=CONFIG.get("temperature", 0.3),
+            stream=True,
+        )
+
+        partial_message = ""
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                partial_message = partial_message + chunk.choices[0].delta.content
+                yield partial_message
+
+
+if __name__ == "__main__":
+    client = OpenAI(api_key="EMPTY", base_url=CONFIG["base_url"])
+    system_prompt = "You are JUST-OS, a useful assistant for all things Open Science."
+
+    index = BGEM3Index.load_from_disk(
+        "storage_m3", weights_for_different_modes=[0.6, 0.4, 0.0]
+    )
+
+    rag_wrapper = RagWrapper(client, system_prompt, index.as_retriever())
+
+    gr.ChatInterface(
+        rag_wrapper.predict,
+        title="JUST-OS",
+        examples=[
+            "What is preregistration and why is it important?",
+            "Where can I preregister my research?",
+            "How does open science reshape the future of interdisciplinary and collaborative research?",
+        ],
+    ).queue().launch(show_api=False, server_port=CONFIG["server_port"])
