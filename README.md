8743e10f14f0af9dd2b7ad47e40e2aa09f8088a8
Michiel van der Ree <michiel.van.der.ree@rug.nl>
2024-12-19 15:29:22 +0100

    Small fixes (#2)

--- README.md
+++ README.md
@@ -2,20 +2,24 @@
 
 This repository contains the source code for the [JUST-OS](https://just-open-science.github.io/) project.
 
+
 ## Installation
 
-1. Install Poetry (if not already installed):
-```bash
-pip install poetry
-```
+1. Install uv according to the instructions for your platform [here](https://docs.astral.sh/uv/getting-started/installation/)
 
 2. Clone the repository:
 ```bash
 git clone https://github.com/just-open-science/JUST-OS.git
 cd JUST-OS
 ```
+3. Download the vector store from [here](https://drive.google.com/file/d/1kgAvRN3OQ0UjkFmBXXtJcHIiFbcslJIl/view?usp=drive_link) and extract it to `data/interim`.
 
-3. Install dependencies:
+4. Create an `.env` file with your UG-LLM api key like
+```
+RUGLLM_API_KEY=<your key>
+``` 
+
+5. Start the app with:
 ```bash
-poetry install
+uv run app.py
 ```
--- app.py
+++ app.py
@@ -82,16 +82,12 @@ generation_instance_prompts_w_references = (
 
 
 def post_process_response(raw_response: str) -> str:
-    return (
-        [
-            t.split("[Response_End]")[0]
-            for t in raw_response.split("[Response_Start]")
-            if "[Response_End]" in t
-        ][0]
-        if "[Response_End]" in raw_response
-        else raw_response
-    )
-
+    response = raw_response
+    if "[Response_Start]" in response:
+        response = response.split("[Response_Start]", 1)[1]
+    if "[Response_End]" in response:
+        response = response.split("[Response_End]")[0]
+    return response
 
 class RagWrapper:
     def __init__(self, client, system_prompt, retriever):
@@ -128,7 +124,26 @@ class RagWrapper:
 
 if __name__ == "__main__":
     client = OpenAI(
-        api_key=os.getenv("LITELLM_PROXY_API_KEY"), base_url=CONFIG["base_url"]
+        api_key=os.getenv("RUGLLM_API_KEY"), base_url=CONFIG["base_url"]
+    )
+    system_prompt = (
+        "You are a helpful AI assistant for scientific literature review. "
+        "Please carefully follow user's instruction and help them to understand the most recent papers."
+    )
+
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+
+    persist_dir = "data/interim/vs_241218_bge-small-en-v1.5"
+    vector_store = FaissVectorStore.from_persist_dir(persist_dir)
+    storage_context = StorageContext.from_defaults(
+        vector_store=vector_store, persist_dir=persist_dir
+    )
+    index = load_index_from_storage(
+        storage_context=storage_context, embed_model=embed_model
+    )
+
+    rag_wrapper = RagWrapper(
+        client, system_prompt, index.as_retriever(similarity_top_k=7)
     )
     system_prompt = (
         "You are a helpful AI assistant for scientific literature review. "
